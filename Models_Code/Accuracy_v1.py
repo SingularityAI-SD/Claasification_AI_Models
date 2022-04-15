4020df140c1d46736837c5fc653c5d3f054cfb33
Rishit Kapoor <91477786+SingularityAI-SD@users.noreply.github.com>
2022-04-16 01:51:15 +0530

    Delete Models_Code directory

--- Models_Code/Accuracy_v1.py
+++
@@ -1,9 +0,0 @@
-# Accuracy evaluation metrics
-
-
-def accuracy_ver1(y_true,y_pred):
-    sum =0
-    for i in range(0,len(y_true)):
-        if y_true[i]==y_pred[i]:
-            sum+=1
-    return sum/len(y_true)
--- Models_Code/Precision.py
+++
@@ -1,9 +0,0 @@
-# Accuracy evaluation metrics using Precesion
-
-from Accuracy_v2 import true_positive,false_positive
-
-# use formulea-> tp/(tp+fp)
-def precesion(y_true,y_pred):
-    tp = true_positive(y_true,y_pred)
-    fp = false_positive(y_true,y_pred)
-    return (tp/(tp+fp))
